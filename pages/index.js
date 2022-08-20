274029de7daf0dacc28d82eda5e6ceae1b64d836
Suraj Kumar <znsuraj7@gmail.com>
2022-08-20 12:57:01 +0530

    added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className='mx-4 bg-red-300'>hello world</h1>
+    <h1 className='mx-4 bg-red-300 text-3xl'>hello suraj world</h1>
     </>
   )
 }
